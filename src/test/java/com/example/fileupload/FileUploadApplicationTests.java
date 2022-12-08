fef88fd6ee1d472838fc28c8e432903788496df8
xanqus <sbk98031816@gmail.com>
2022-12-08 17:16:44 +0900

    aws s3 bucket 파일 삭제 테스트케이스 구현

--- src/test/java/com/example/fileupload/FileUploadApplicationTests.java
+++ src/test/java/com/example/fileupload/FileUploadApplicationTests.java
@@ -1,13 +1,24 @@
 package com.example.fileupload;
 
+import com.amazonaws.services.s3.AmazonS3;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FileUploadApplicationTests {
 
+	@Value("${cloud.aws.s3.bucket}")
+	private String bucket;
+
+	@Autowired
+	private AmazonS3 amazonS3;
+
+
 	@Test
 	void contextLoads() {
+		amazonS3.deleteObject(bucket, "filename");
 	}
 
 }
